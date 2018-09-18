f219d54d0f9dc2cca513c466446b270c52fc3c6e
Wilker Lucio <wilkerlucio@gmail.com>
2018-09-18 10:32:33 -0300

    ES6 style detect element size

--- src/core/com/wsscode/pathom/viz/trace.cljs
+++ src/core/com/wsscode/pathom/viz/trace.cljs
@@ -1,6 +1,6 @@
 (ns com.wsscode.pathom.viz.trace
   (:require ["./d3-trace" :refer [renderPathomTrace updateTraceSize]]
-            ["./detect-element-size"]
+            ["./detect-element-size" :refer [addResizeListener]]
             [fulcro.client.localized-dom :as dom]
             [fulcro.client.primitives :as fp]
             [goog.object :as gobj]
@@ -140,7 +140,7 @@
    :componentDidMount
    (fn []
      (render-trace this)
-     (js/addResizeListener (gobj/get this "svgContainer") #(recompute-trace-size this)))
+     (addResizeListener (gobj/get this "svgContainer") #(recompute-trace-size this)))
 
    :componentDidUpdate
    (fn [prev-props _]
@@ -153,39 +153,3 @@
     (dom/svg {:ref #(gobj/set this "svg" %)})))
 
 (def d3-trace (fp/factory D3Trace))
-
-(fp/defsc TraceView [this {:keys [expanded?]}]
-  {:ident [:trace-id :trace-id]
-   :query [:trace-id :trace-data :expanded?]
-   :css   [[:.container {:width  "100%"
-                         :height "300px"
-                         :border "2px solid #da3939"}]
-           [:.resized {:width  "80%"
-                       :height "500px"}]]
-   :componentDidMount
-          (fn []
-            (let [trace (-> this fp/props :trace-data)
-                  container (gobj/get this "svgContainer")
-                  svg (gobj/get this "svg")]
-              (gobj/set this "renderedData"
-                (renderPathomTrace svg
-                  (clj->js {:svgWidth  (gobj/get container "clientWidth")
-                            :svgHeight (gobj/get container "clientHeight")
-                            :data      trace})))))
-
-   :componentDidUpdate
-          (fn [_ _]
-            (let [container (gobj/get this "svgContainer")]
-              (updateTraceSize
-                (doto (gobj/get this "renderedData")
-                  (gobj/set "svgWidth" (gobj/get container "clientWidth"))
-                  (gobj/set "svgHeight" (gobj/get container "clientHeight"))))))}
-  (dom/div
-    (dom/button {:onClick #(fm/toggle! this :expanded?)} "Toggle Size")
-    (dom/br)
-    (dom/br)
-    (dom/div :.container {:ref     #(gobj/set this "svgContainer" %)
-                          :classes [(if expanded? :.resized)]}
-      (dom/svg {:ref #(gobj/set this "svg" %)}))))
-
-(def trace-view (fp/factory TraceView))
