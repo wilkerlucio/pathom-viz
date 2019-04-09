a7ae71cd1650ef851238e9b6c8c26ceb2cd8b3d3
Wilker Lucio <wilkerlucio@gmail.com>
2019-04-09 19:53:51 -0300

    Bump d3 on cljs deps

--- src/core/deps.cljs
+++ src/core/deps.cljs
@@ -1,4 +1,4 @@
 {:npm-deps {:codemirror          "^5.40.0"
-            :d3                  "^5.7.0"
+            :d3                  "^5.8.0"
             :parinfer            "^3.12.0"
             :parinfer-codemirror "^1.4.2"}}
