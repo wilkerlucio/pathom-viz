525f864a7b0b5af98726fadbf43365b0257b4211
Wilker Lucio <wilkerlucio@gmail.com>
2019-12-07 11:14:44 -0300

    Electron app setup

---
+++ src/electron/com/wsscode/pathom/viz/electron/background/main.cljs
@@ -0,0 +1,26 @@
+(ns com.wsscode.pathom.viz.electron.background.main
+  (:require
+    [com.wsscode.pathom.viz.electron.background.server :as server]
+    ["electron" :as electron]
+    ["path" :as path]
+    ["url" :as url]))
+
+(defonce contents (atom nil))
+
+(defn create-window []
+  (let [win (electron/BrowserWindow. #js {:width          800
+                                          :height         600
+                                          :webPreferences #js {:nodeIntegration true}})]
+    (.loadURL win (url/format #js {:pathname (path/join js/__dirname ".." ".." "index.html")
+                                   :protocol "file:"
+                                   :slashes  "true"}))
+    (.. win -webContents openDevTools)
+    (reset! contents (.-webContents win))))
+
+(defn init []
+  (js/console.log "start")
+  (electron/app.on "ready" create-window)
+  (server/start! {:content-atom contents}))
+
+(defn after-load []
+  (js/console.log "Done reloading"))
---
+++ src/electron/com/wsscode/pathom/viz/electron/renderer/main.cljs
@@ -0,0 +1,7 @@
+(ns com.wsscode.pathom.viz.electron.renderer.main
+  (:require))
+
+(defn after-load []
+  (js/console.log "Done reloading"))
+
+(js/console.log "Page started")
