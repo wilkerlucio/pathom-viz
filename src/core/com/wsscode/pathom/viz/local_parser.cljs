76992e2502549fed6493f913cb5211372777e6a7
Wilker Lucio <wilkerlucio@gmail.com>
2020-04-08 00:34:01 -0300

    Remove singleton demo parser

--- src/core/com/wsscode/pathom/viz/local_parser.cljs
+++ src/core/com/wsscode/pathom/viz/local_parser.cljs
@@ -1,13 +1,12 @@
 (ns com.wsscode.pathom.viz.local-parser
   "Combined local parser to use that fulfills the Pathom Viz remote demands."
-  (:require [com.wsscode.pathom.viz.aux.demo-parser :as demo-parser]
-            [com.wsscode.pathom.viz.client-parser :as cp]
+  (:require [com.wsscode.pathom.viz.client-parser :as cp]
             [com.wsscode.pathom.viz.query-editor :as query.editor]
             [com.wsscode.pathom.core :as p]
             [com.wsscode.pathom.connect :as pc]))
 
 (defonce client-parsers
-  (atom {:singleton demo-parser/parser}))
+  (atom {}))
 
 (def registry
   [cp/registry query.editor/registry])
