ac4205453cd6051e38c136b9dc18c0f9ea7d8e10
Wilker Lucio <wilkerlucio@gmail.com>
2021-01-23 16:51:24 -0300

    Log batch duration, and include run stats in nodes

--- src/core/com/wsscode/pathom/viz/timeline.cljs
+++ src/core/com/wsscode/pathom/viz/timeline.cljs
@@ -30,58 +30,69 @@
          start     (or start
                        (-> run-stats :com.wsscode.pathom3.connect.runner.stats/process-run-start-ms))]
      (if run-stats
-       {:start    (if start
-                    (- (:com.wsscode.pathom3.connect.runner.stats/process-run-start-ms run-stats) start)
-                    0)
-        :name     (str (peek path))
-        :path     path
-        :details  [{:event    "Make plan"
-                    :start    (- (::pcr/compute-plan-run-start-ms run-stats) start)
-                    :duration (::pcr/compute-plan-run-duration-ms run-stats)
-                    }]
-        :duration (:com.wsscode.pathom3.connect.runner.stats/process-run-duration-ms run-stats)
-        :children (into []
-                        (keep
-                          (fn [{::pcp/keys [nested-process node-id]
-                                ::pcr/keys [node-run-start-ms
-                                            node-run-duration-ms
-                                            resolver-run-start-ms
-                                            resolver-run-duration-ms]
-                                ::keys     [span-label]
-                                ::pco/keys [op-name]
-                                :as        node}]
-                            (if node-run-duration-ms
-                              (let [path' (conj path node-id)]
-                                (cond-> {:path     path'
-                                         :node     (volatile! node)
-                                         :start    (- node-run-start-ms start)
-                                         :duration node-run-duration-ms
-                                         :name     (if op-name (str op-name) (str span-label))
-                                         :details  (cond-> []
-                                                     resolver-run-duration-ms
-                                                     (conj
-                                                       {:event    "Run resolver"
-                                                        :start    (- resolver-run-start-ms start)
-                                                        :duration resolver-run-duration-ms}))}
-                                  (seq nested-process)
-                                  (assoc :children
-                                         (into []
-                                               (mapcat (fn [attr]
-                                                         (let [val (get data attr)]
-                                                           (cond
-                                                             (map? val)
-                                                             (compute-timeline-tree (get data attr)
-                                                               [(conj path' attr)]
-                                                               start)
+       {:start     (if start
+                     (- (:com.wsscode.pathom3.connect.runner.stats/process-run-start-ms run-stats) start)
+                     0)
+        :name      (str (peek path))
+        :run-stats (volatile! run-stats)
+        :path      path
+        :details   [{:event    "Make plan"
+                     :start    (- (::pcr/compute-plan-run-start-ms run-stats) start)
+                     :duration (::pcr/compute-plan-run-duration-ms run-stats)}]
+        :duration  (:com.wsscode.pathom3.connect.runner.stats/process-run-duration-ms run-stats)
+        :children  (into []
+                         (keep
+                           (fn [{::pcp/keys [nested-process node-id]
+                                 ::pcr/keys [node-run-start-ms
+                                             node-run-duration-ms
+                                             resolver-run-start-ms
+                                             resolver-run-duration-ms
+                                             batch-run-start-ms
+                                             batch-run-duration-ms]
+                                 ::keys     [span-label]
+                                 ::pco/keys [op-name]
+                                 :as        node}]
+                             (if node-run-duration-ms
+                               (let [path' (conj path node-id)]
+                                 (cond-> {:path      path'
+                                          :node      (volatile! node)
+                                          :run-stats (volatile! run-stats)
+                                          :start     (- node-run-start-ms start)
+                                          :duration  node-run-duration-ms
+                                          :name      (if op-name (str op-name) (str span-label))
+                                          :details   (cond-> []
+                                                       resolver-run-duration-ms
+                                                       (conj
+                                                         {:event    "Run resolver"
+                                                          :start    (- resolver-run-start-ms start)
+                                                          :duration resolver-run-duration-ms
+                                                          :style    {:fill "#af9df4"}})
 
-                                                             (sequential? val)
-                                                             (into []
-                                                                   (map-indexed #(compute-timeline-tree %2
-                                                                                   (conj path' %)
-                                                                                   start))
-                                                                   val)
+                                                       batch-run-duration-ms
+                                                       (conj
+                                                         {:event    "Batch run"
+                                                          :start    (- batch-run-start-ms start)
+                                                          :duration batch-run-duration-ms
+                                                          :style    {:fill "#6ac5ec"}}))}
+                                   (seq nested-process)
+                                   (assoc :children
+                                          (into []
+                                                (mapcat (fn [attr]
+                                                          (let [val (get data attr)]
+                                                            (cond
+                                                              (map? val)
+                                                              (compute-timeline-tree (get data attr)
+                                                                [(conj path' attr)]
+                                                                start)
 
-                                                             :else
-                                                             []))))
-                                               nested-process)))))))
-                        (->> run-stats ::pcp/nodes vals (sort-by ::pcr/node-run-start-ms)))}))))
+                                                              (sequential? val)
+                                                              (into []
+                                                                    (map-indexed #(compute-timeline-tree %2
+                                                                                    (conj path' %)
+                                                                                    start))
+                                                                    val)
+
+                                                              :else
+                                                              []))))
+                                                nested-process)))))))
+                         (->> run-stats ::pcp/nodes vals (sort-by ::pcr/node-run-start-ms)))}))))
