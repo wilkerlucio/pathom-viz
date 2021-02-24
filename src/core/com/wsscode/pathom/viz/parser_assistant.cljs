5c15db53a8001edb5aab95305389098d55f9f080
Wilker Lucio <wilkerlucio@gmail.com>
2021-02-24 10:27:44 -0300

    Style links and disabled buttons

--- src/core/com/wsscode/pathom/viz/parser_assistant.cljs
+++ src/core/com/wsscode/pathom/viz/parser_assistant.cljs
@@ -198,16 +198,16 @@
               (dom/div
                 (dom/div :.large (ui/gc :.center) "Connect a parser")
                 (dom/div "Not sure what to do to? "
-                  (dom/a {:href    "#"
-                          :onClick #(do
-                                      (.preventDefault %)
-                                      (fc/transact! this [(mf/open-external {:url "https://roamresearch.com/#/app/wsscode/page/RG9C93Sip"})]))}
+                  (ui/link {:href    "#"
+                            :onClick #(do
+                                        (.preventDefault %)
+                                        (fc/transact! this [(mf/open-external {:url "https://roamresearch.com/#/app/wsscode/page/RG9C93Sip"})]))}
                     "Docs for Pathom 2")
                   " | "
-                  (dom/a {:href    "#"
-                          :onClick #(do
-                                      (.preventDefault %)
-                                      (fc/transact! this [(mf/open-external {:url "https://pathom3.wsscode.com/docs/debugging/#debug-with-pathom-viz"})]))}
+                  (ui/link {:href    "#"
+                            :onClick #(do
+                                        (.preventDefault %)
+                                        (fc/transact! this [(mf/open-external {:url "https://pathom3.wsscode.com/docs/debugging/#debug-with-pathom-viz"})]))}
                     "Docs for Pathom 3")
                   ".")))))))))
 
