9eb821b5fe60064c2ba2d1f98d3ae77155bf6b89
Wilker Lucio <wilkerlucio@gmail.com>
2022-02-15 13:27:58 -0300

    Fix lint issues

--- src/electron/com/wsscode/pathom/viz/electron/background/parser.cljs
+++ src/electron/com/wsscode/pathom/viz/electron/background/parser.cljs
@@ -1,11 +1,9 @@
 (ns com.wsscode.pathom.viz.electron.background.parser
   (:require ["electron" :refer [app]]
-            [com.wsscode.pathom3.connect.built-in.plugins :as pbip]
             [com.wsscode.pathom3.connect.indexes :as pci]
             [com.wsscode.pathom3.connect.operation :as pco]
             [com.wsscode.pathom3.connect.planner :as pcp]
             [com.wsscode.pathom3.interface.async.eql :as p.a.eql]
-            [com.wsscode.pathom3.plugin :as p.plugin]
             [promesa.core :as p]))
 
 (pco/defresolver app-version []
